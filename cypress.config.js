23648f06571c4f433aabbd0b5db84f82bd37ab66
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-08-01 22:35:09 -0300

    Add defaultCommandTimeout para 89000 milissegundos

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,16 @@
+// const { defineConfig } = require('cypress')
+
+// module.exports = defineConfig({
+//   e2e: {
+//     // We've imported your old cypress plugins here.
+//     // You may want to clean this up later by importing these.
+//     setupNodeEvents(on, config) {
+//       return require('./cypress/plugins/index.js')(on, config)
+//     },
+//     baseUrl: 'http://34.171.117.131/',
+//   },
+// })
+
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
@@ -9,4 +22,5 @@ module.exports = defineConfig({
     },
     baseUrl: 'http://34.171.117.131/',
   },
+  defaultCommandTimeout: 89000,
 })
